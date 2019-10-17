aec147ce524cad17abd0aa559823608ef6ff5337
ahmadizade <hr.ahmadi689@yahoo.com>
2019-10-17 13:27:28 +0330

    step-5

---
+++ js/my-style.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#online").mouseover(function(){
+        $("#anime").animate({width: '100%',opacity: '1'},4000);
+    });
+    // $("#online").mouseleave(function(){
+    //     $("#anime").animate({width: '0',opacity: '0.5',},1000);
+    // });
+});
