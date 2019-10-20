128559a19ca127419cdd6722516b558bcca9075b
ahmadizade <hr.ahmadi689@yahoo.com>
2019-10-20 17:04:27 +0330

    step-5

--- js/my-style.js
+++ js/my-style.js
@@ -6,3 +6,8 @@ $(document).ready(function(){
     //     $("#anime").animate({width: '0',opacity: '0.5',},1000);
     // });
 });
+$(document).ready(function(){
+    $(".footer-header").click(function(){
+        $(".footer-ul").toggle(1000);
+    });
+});
