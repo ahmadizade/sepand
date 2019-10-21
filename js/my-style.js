43909a304a6fdf04bbc5f80b5a8197c2e9a008d6
ahmadizade <hr.ahmadi689@yahoo.com>
2019-10-21 17:09:09 +0330

    step-6

--- js/my-style.js
+++ js/my-style.js
@@ -2,12 +2,25 @@ $(document).ready(function(){
     $("#online").mouseover(function(){
         $("#anime").animate({width: '100%',opacity: '1'},4000);
     });
-    // $("#online").mouseleave(function(){
-    //     $("#anime").animate({width: '0',opacity: '0.5',},1000);
-    // });
-});
-$(document).ready(function(){
-    $(".footer-header").click(function(){
-        $(".footer-ul").toggle(1000);
+   });
+    $("#footer-top-1").click(function(){
+        $("#footer-ul-1 li").toggle(1000);
+    });
+    $("#footer-top-2").click(function(){
+        $("#footer-ul-2 li").toggle(1000);
+    });
+    $("#footer-top-3").click(function(){
+        $("#footer-ul-3 li").toggle(1000);
+    });
+    $("#footer-top-4").click(function(){
+        $("#footer-ul-4 li").toggle(1000);
+    });
+    $("#footer-top-5").click(function(){
+        $("#footer-ul-5 li").toggle(1000);
+    });
+
+$(document).ready(function() {
+    $("#footer-top-1").click(function () {
+        $(#degree).toggleClass('rotate');
     });
 });
