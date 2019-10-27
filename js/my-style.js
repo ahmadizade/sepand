da86e301565965e58244c775595d782efb1437ba
Administrator <hr.ahmadi689@yahoo.com>
2019-10-27 11:46:43 +0330

    home tatil

--- js/my-style.js
+++ js/my-style.js
@@ -3,21 +3,30 @@ $(document).ready(function(){
         $("#anime").animate({width: '100%',opacity: '1'},4000);
     });
    });
+
+
     $("#footer-top-1").click(function(){
-        $("#footer-ul-1 li").toggle(1000);
-    });
-    $("#footer-top-2").click(function(){
-        $("#footer-ul-2 li").toggle(1000);
-    });
-    $("#footer-top-3").click(function(){
-        $("#footer-ul-3 li").toggle(1000);
-    });
-    $("#footer-top-4").click(function(){
-        $("#footer-ul-4 li").toggle(1000);
-    });
-    $("#footer-top-5").click(function(){
-        $("#footer-ul-5 li").toggle(1000);
-    });
+        $(".footer-ul").toggle(
+            function(){$(this).css({"height": "900px"});})
+            });
+
+
+
+    // $("#footer-top-1").click(function(){
+    //     $("#footer-ul-1 li").toggle(1000);
+    // });
+    // $("#footer-top-2").click(function(){
+    //     $("#footer-ul-2 li").toggle(1000);
+    // });
+    // $("#footer-top-3").click(function(){
+    //     $("#footer-ul-3 li").toggle(1000);
+    // });
+    // $("#footer-top-4").click(function(){
+    //     $("#footer-ul-4 li").toggle(1000);
+    // });
+    // $("#footer-top-5").click(function(){
+    //     $("#footer-ul-5 li").toggle(1000);
+    // });
     // $(".visabox").mouseover(function(){
 //     //     $(".statusbox").animate({height:'100%',color:'gold'},3000)
 //     // });
