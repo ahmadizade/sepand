ed4690c42b653ed42f6c8e2909b779ce7f2132b2
Administrator <hr.ahmadi689@yahoo.com>
2019-10-27 21:38:32 +0330

    responsive ok

--- js/my-style.js
+++ js/my-style.js
@@ -5,37 +5,35 @@ $(document).ready(function(){
    });
 
 
-    $("#footer-top-1").click(function(){
-        $(".footer-ul").toggle(
-            function(){$(this).css({"height": "900px"});})
-            });
+// $( window ).on( "load", function() {
+// $("#footer-top-1").onload(function(){
+//     $("#footer-ul-1 li").css("color", "black")
+//         .slideUp(2000);
+// });
+// });
 
-
-
+    //
     // $("#footer-top-1").click(function(){
-    //     $("#footer-ul-1 li").toggle(1000);
+    //     $("#footer-ul-1 li").css("color", "red")
+    //         .slideUp(2000)
+    //         .slideDown(2000);
     // });
-    // $("#footer-top-2").click(function(){
-    //     $("#footer-ul-2 li").toggle(1000);
-    // });
-    // $("#footer-top-3").click(function(){
-    //     $("#footer-ul-3 li").toggle(1000);
-    // });
-    // $("#footer-top-4").click(function(){
-    //     $("#footer-ul-4 li").toggle(1000);
-    // });
-    // $("#footer-top-5").click(function(){
-    //     $("#footer-ul-5 li").toggle(1000);
-    // });
-    // $(".visabox").mouseover(function(){
-//     //     $(".statusbox").animate({height:'100%',color:'gold'},3000)
-//     // });
-//     // $(".visabox").mouseleave(function(){
-//     //     $(".statusbox").animate({height:'45px',color:'gold'},3000)
-//     // });
 
-//     $(document).ready(function(){
-// $("#footer-top-1").click(function(){
-//     $(".footer-header").toggleClass('rotated');
-// });
-// });
+
+
+
+    $("#footer-top-1").click(function(){
+        $("#footer-ul-1 li").toggle(1000);
+    });
+    $("#footer-top-2").click(function(){
+        $("#footer-ul-2 li").toggle(1000);
+    });
+    $("#footer-top-3").click(function(){
+        $("#footer-ul-3 li").toggle(1000);
+    });
+    $("#footer-top-4").click(function(){
+        $("#footer-ul-4 li").toggle(1000);
+    });
+    $("#footer-top-5").click(function(){
+        $("#footer-ul-5 li").toggle(1000);
+    });
